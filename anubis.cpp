e185b5559630c203fa715161eca8cff75d5681a4
Oinotna9 <ntonio9@hotmail.com>
2015-12-14 20:31:43 +0100

    Funcionacreo

--- anubis.cpp
+++ anubis.cpp
@@ -3,23 +3,23 @@
 USING_NS_CC;
 
 Sprite *anubis::createAnimation(int numeron, bool load)
-{   
-	 
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-	 
-	
+{
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+
 	auto cacher = SpriteFrameCache::getInstance();
 	cacher->addSpriteFramesWithFile("anoobisSpritesheet.plist");
-	
-	
-	auto someSprite = Sprite::createWithSpriteFrameName("anoobis1.png");
+
+
+	auto someSprite = Sprite::createWithSpriteFrameName("anoobisIdle1.png");
 
 	Vector<SpriteFrame*> frames;
 	for (int i = 1; i < 2; i++)
 	{
 		std::stringstream ss;
-		ss << "anoobis" << i << ".png";
+		ss << "anoobisIdle" << i << ".png";
 		frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
 	}
 	// play the animation
@@ -27,104 +27,104 @@ Sprite *anubis::createAnimation(int numeron, bool load)
 	//asomeSprite->runAction(Animate::create(anim1));
 
 
-	if (load == true){
+	if (load == true) {
 
-		if (numeron==1){
+		if (numeron == 1) {
 			// load all the animation frames into an array
 			Vector<SpriteFrame*> frames;
-			for (int i = 4; i <= 7; i++)
+			for (int i = 1; i <= 4; i++)
 			{
 				std::stringstream ss;
-				ss << "anoobis" << i << ".png";
+				ss << "anoobisSpin" << i << ".png";
 				frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
 			}
-						// play the animation
-				Animation* anim = Animation::createWithSpriteFrames(frames, 0.05f);
-				someSprite->setPosition(260, 100);
+			// play the animation
+			Animation* anim = Animation::createWithSpriteFrames(frames, 0.05f);
+			someSprite->setPosition(260, 100);
 
-				// create a few actions.
-			
-				auto canim = Repeat::create(Animate::create(anim),5);
+			// create a few actions.
 
-				auto repite = Repeat::create(Animate::create(anim1),99999);
+			auto canim = Repeat::create(Animate::create(anim), 5);
 
-				// create a sequence with the actions and callbacks
-				auto seq = Sequence::create(canim, repite, nullptr);
+			auto repite = Repeat::create(Animate::create(anim1), 99999);
 
-				// run it
-				someSprite->runAction(seq);
+			// create a sequence with the actions and callbacks
+			auto seq = Sequence::create(canim, repite, nullptr);
+
+			// run it
+			someSprite->runAction(seq);
 
 		}
-		if (numeron==2){
+		if (numeron == 2) {
 			// load all the animation frames into an array
 			Vector<SpriteFrame*> frames;
-			for (int i = 8; i <= 12; i++)
+			for (int i = 1; i <= 6; i++)
 			{
 				std::stringstream ss;
-				ss << "anoobis" << i << ".png";
+				ss << "anoobisDown" << i << ".png";
 				frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
 			}
-						// play the animation
-				Animation* anim = Animation::createWithSpriteFrames(frames, 0.3f);
-				someSprite->setPosition(260, 100);
+			// play the animation
+			Animation* anim = Animation::createWithSpriteFrames(frames, 0.3f);
+			someSprite->setPosition(260, 100);
 
-				// create a few actions.
-			
-				auto canim = Animate::create(anim);
+			// create a few actions.
 
-				auto repite = Repeat::create(Animate::create(anim1),99999);
+			auto canim = Animate::create(anim);
 
-				// create a sequence with the actions and callbacks
-				auto seq = Sequence::create(canim, repite, nullptr);
+			auto repite = Repeat::create(Animate::create(anim1), 99999);
 
-				// run it
-				someSprite->runAction(seq);
+			// create a sequence with the actions and callbacks
+			auto seq = Sequence::create(canim, repite, nullptr);
 
-			}
+			// run it
+			someSprite->runAction(seq);
+
+		}
 	}
 
-	else{
+	else {
 		//if (numeron==2){
-				// load all the animation frames into an array
-				Vector<SpriteFrame*> frames;
-				for (int i = 2; i <= 3; i++)
-				{
-					std::stringstream ss;
-					ss << "anoobis" << i << ".png";
-					frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
-				}
-							// play the animation
-					Animation* anim = Animation::createWithSpriteFrames(frames, 0.2f);
-					someSprite->setPosition(260, 100);
+		// load all the animation frames into an array
+		Vector<SpriteFrame*> frames;
+		for (int i = 1; i <= 2; i++)
+		{
+			std::stringstream ss;
+			ss << "anoobisUp" << i << ".png";
+			frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
+		}
+		// play the animation
+		Animation* anim = Animation::createWithSpriteFrames(frames, 0.2f);
+		someSprite->setPosition(260, 100);
 
-					// create a few actions.
-			
-					auto canim = Animate::create(anim);
+		// create a few actions.
 
-					auto repite = Repeat::create(Animate::create(anim1),99999);
+		auto canim = Animate::create(anim);
 
-					// create a sequence with the actions and callbacks
-					auto seq = Sequence::create(canim, repite, nullptr);
+		auto repite = Repeat::create(Animate::create(anim1), 99999);
 
-					// run it
-					someSprite->runAction(seq);
+		// create a sequence with the actions and callbacks
+		auto seq = Sequence::create(canim, repite, nullptr);
+
+		// run it
+		someSprite->runAction(seq);
 
 		//}
-		
-	}
-	
-	if (numeron == 0){
-
-		
-			someSprite->setPosition(260, 100);	
-			someSprite->runAction(RepeatForever::create(Animate::create(anim1)));
-			
 
 	}
-	
-	
+
+	if (numeron == 0) {
+
+
+		someSprite->setPosition(260, 100);
+		someSprite->runAction(RepeatForever::create(Animate::create(anim1)));
+
+
+	}
+
+
 	return someSprite;
-	
-	}
+
+}
 
 
